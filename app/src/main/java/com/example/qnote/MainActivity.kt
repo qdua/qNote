aa4d428340aaf07663a8f305c7211cf454c5efb1
Sparklingman <hi@qd.ua>
2021-04-08 12:12:02 +0300

    v 1.03

--- app/src/main/java/com/example/qnote/MainActivity.kt
+++ app/src/main/java/com/example/qnote/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.qnote
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var pInfo: PackageInfo? = null
+        try {
+            pInfo = packageManager.getPackageInfo(packageName, 0)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        val version = pInfo!!.versionName
+        val versionText = findViewById<View>(R.id.copy) as TextView
+        versionText.text = "© 2021 All Rights Reserved v. $version"
     }
 }
